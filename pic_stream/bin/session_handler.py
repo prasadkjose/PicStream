ecc009c343f297acddcb0da1897e837e6d88e643
prasadkjose <prasadkjose@gmail.com>
2025-01-16 21:27:43 +0100

    Init session handler

---
+++ pic_stream/bin/session_handler.py
@@ -0,0 +1,17 @@
+""" Module that initiates and handles the user session a successful oAuth request."""
+
+
+class UserSessionHandler:
+    """ """
+
+    def __init__(self, session: dict):
+        """Takes in the Json Response from the API"""
+        self.raw_session = session
+
+    def parse_session(self):
+        """Takes and validates the PickingSession Json Response from the API and parses it into it's components."""
+        # TODO: validate session with pydantic
+        for k, v in self.raw_session.items():
+            setattr(self, k, v)
+
+        
